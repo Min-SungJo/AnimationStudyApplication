802a448d19638c3dd53227043844496a267360bb
Min-SungJo <123or@naver.com>
2022-12-10 22:49:17 +0900

    재생버튼 추가, 버튼 클릭하면 하트 애니메이션 재생

--- app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
+++ app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.animationstudyapplication
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnHeart = findViewById<Button>(R.id.btnHeart)
+        val animHeart = findViewById<LottieAnimationView>(R.id.animHeart)
+
+        btnHeart.setOnClickListener {
+            Log.d("button","Click!")
+
+            // animation custom
+            val animator = ValueAnimator
+                .ofFloat(0f,1f) // 0f ~1f -> animation 시작과 끝지점(0%~100%) 설정
+                .setDuration(1000) // 지속시간 1초
+
+            animator.addUpdateListener {
+                animHeart.setProgress(
+                    it.getAnimatedValue() as Float
+                )
+            }
+            animator.start()
+        }
     }
 }
