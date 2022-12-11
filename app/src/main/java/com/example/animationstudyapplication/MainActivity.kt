dae062266e31f94ce9317b001be6e58108a9fd79
Min-SungJo <123or@naver.com>
2022-12-11 22:35:23 +0900

    재생버튼 추가, 버튼 클릭하면 폭죽 애니메이션 재생

--- app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
+++ app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
@@ -13,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnHeart = findViewById<Button>(R.id.btnHeart)
+        val btnConfetti = findViewById<Button>(R.id.btnConfetti)
         val animHeart = findViewById<LottieAnimationView>(R.id.animHeart)
+        val animConfetti = findViewById<LottieAnimationView>(R.id.animConfetti)
 
         btnHeart.setOnClickListener {
             Log.d("button","Click!")
@@ -25,6 +27,21 @@ class MainActivity : AppCompatActivity() {
 
             animator.addUpdateListener {
                 animHeart.setProgress(
+                    it.getAnimatedValue() as Float
+                )
+            }
+            animator.start()
+        }
+        btnConfetti.setOnClickListener {
+            Log.d("button","Click!")
+
+            // animation custom
+            val animator = ValueAnimator
+                .ofFloat(0f,1f) // 0f ~1f -> animation 시작과 끝지점(0%~100%) 설정
+                .setDuration(1000) // 지속시간 1초
+
+            animator.addUpdateListener {
+                animConfetti.setProgress(
                     it.getAnimatedValue() as Float
                 )
             }
