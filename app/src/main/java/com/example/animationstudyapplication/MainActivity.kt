3342abba9f022a2e788d703078c85cb1172c818b
Min-SungJo <123or@naver.com>
2022-12-11 23:07:01 +0900

    heart 버튼 클릭하면 회색하트/붉은하트 변환

--- app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
+++ app/src/main/java/com/example/animationstudyapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
 
@@ -15,22 +16,45 @@ class MainActivity : AppCompatActivity() {
         val btnHeart = findViewById<Button>(R.id.btnHeart)
         val btnConfetti = findViewById<Button>(R.id.btnConfetti)
         val animHeart = findViewById<LottieAnimationView>(R.id.animHeart)
+        val animHeartGray = findViewById<LottieAnimationView>(R.id.animHeartGray)
         val animConfetti = findViewById<LottieAnimationView>(R.id.animConfetti)
+
+        var booleanHeart:Boolean=false
 
         btnHeart.setOnClickListener {
             Log.d("button","Click!")
 
-            // animation custom
-            val animator = ValueAnimator
-                .ofFloat(0f,1f) // 0f ~1f -> animation 시작과 끝지점(0%~100%) 설정
-                .setDuration(1000) // 지속시간 1초
+            if(booleanHeart==false) {
+                booleanHeart = true
+                animHeart.visibility=View.VISIBLE
+                animHeartGray.visibility=View.INVISIBLE
+                // animation custom
+                val animator = ValueAnimator
+                    .ofFloat(0f, 1f) // 0f ~1f -> animation 시작과 끝지점(0%~100%) 설정
+                    .setDuration(1000) // 지속시간 1초
 
-            animator.addUpdateListener {
-                animHeart.setProgress(
-                    it.getAnimatedValue() as Float
-                )
+                animator.addUpdateListener {
+                    animHeart.setProgress(
+                        it.getAnimatedValue() as Float
+                    )
+                }
+                animator.start()
+            }else{
+                booleanHeart =false
+                animHeart.visibility=View.INVISIBLE
+                animHeartGray.visibility=View.VISIBLE
+                // animation custom
+                val animator = ValueAnimator
+                    .ofFloat(0f, 1f) // 0f ~1f -> animation 시작과 끝지점(0%~100%) 설정
+                    .setDuration(1000) // 지속시간 1초
+
+                animator.addUpdateListener {
+                    animHeartGray.setProgress(
+                        it.getAnimatedValue() as Float
+                    )
+                }
+                animator.start()
             }
-            animator.start()
         }
         btnConfetti.setOnClickListener {
             Log.d("button","Click!")
